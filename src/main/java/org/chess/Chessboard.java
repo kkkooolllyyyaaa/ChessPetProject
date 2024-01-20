c9e7b6d248ae7902b6dbcbfcaab3b2df384fb652
SuleymanovSergey <98027460+SuleymanovSergey@users.noreply.github.com>
2024-01-20 15:21:12 +0500

    1

--- src/main/java/org/chess/Chessboard.java
+++ src/main/java/org/chess/Chessboard.java
@@ -14,12 +14,21 @@ public class Chessboard {
         pieces.put(coordinates, piece);
     }
 
+    public boolean isSquareEmpty(Coordinates coordinates){
+        return !pieces.containsKey(coordinates);
+    }
+
+    public Piece getPiece(Coordinates coordinates){
+        return pieces.get(coordinates);
+    }
+
     public void setupDefaultPiecePosition {
         for (Horizontal horizontal : Horizontal.values()) {
             setPiece(new Coordinates(horizontal,2), new Pawn(Color.WHITE, new Coordinates(horizontal,2)));
             setPiece(new Coordinates(horizontal,7),new Pawn(Color.BLACK, new Coordinates(horizontal, 7)));
         }
     }
-
-
+    public static void isSquareDark(){
+        return ((coordinates.horizontal.ordinal() + 1)+coordinates.vertical)%2) == 0;
+    }
 }
